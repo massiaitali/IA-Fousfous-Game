9a691041bdfb1812387be23a85109b0fc97e0f40
massi.aitali <massi.aitali@gmail.com>
2017-10-19 10:50:08 +0200

    Initial commit

---
+++ src/fousfous_2/Partie1.java
@@ -0,0 +1,31 @@
+package fousfous_2;
+
+
+public interface Partie1 {
+    /** initialise un plateau à partir d’un fichier texte
+    * @param fileName le nom du fichier à lire
+    */
+    //public void setFromFile(String fileName);
+    /** sauve la configuration d’un plateau dans d’un fichier
+    * @param fileName le nom du fichier à sauvegarder
+    *
+    * Le format doit être compatible avec celui utilisé pour la lecture.
+    */
+    //public void saveToFile(String fileName);
+    /** indique si le coup <move> est valide pour le joueur <player> sur le plateau courant
+    * @param move le coup à jouer sous la forme "A1-B2"
+    * @param player le joueur qui joue (représenté par "noir" ou "blanc")
+    */
+    //public boolean estValide(String move, String player);
+    /** calcule les coups possibles pour le joueur <player> sur le plateau courant
+    * @param player le joueur qui joue (représenté par "noir" ou "blanc")
+    blanc public String[] mouvementsPossibles(String player);
+    /** modifie le plateau en jouant le coup move
+    * @param move le coup à jouer, représenté sous la forme "A1-B2"
+    * @param player le joueur qui joue représenté par "noir" ou "blanc"
+    */
+    //public void play(String move, String player);
+    /** vrai lorsque le plateau correspond à une fin de partie
+    */
+    public boolean finDePartie();
+}
---
+++ src/fousfous_2/PlateauFousFous.java
@@ -0,0 +1,79 @@
+package fousfous_2;
+
+public class PlateauFousFous implements Partie1 {
+    // Initialisation du plateau avce un tableau de cellule 
+    public Cellule[][] Plateau;
+    
+    // Génere le plateau avec les pions blanc et noir
+	public PlateauFousFous() {
+
+		this.Plateau = new Cellule[8][8];
+
+		for (int w = 0; w < 8; w++) {
+			for (int i = 0; i < 8; i++) {
+				if ((w % 2) == 0 && (i % 2) == 1) {
+					this.Plateau[w][i] = new Cellule(w, i, "b");
+				} else if ((w % 2) == 1 && (i % 2) == 0) {
+					this.Plateau[w][i] = new Cellule(w, i, "n");
+				} else {
+					this.Plateau[w][i] = new Cellule(w, i, "-");
+				}
+			}
+		}
+    }
+
+    // Test la fin de partie
+    public boolean finDePartie() {
+		return nbPionBlanc() == 0 || nbPionNoir() == 0;
+	}
+    // Renvoie le nb de pions blanc sur le plateau
+	public int nbPionBlanc() {
+		int nb = 0;
+		for (int i = 0; i < 8; i++) {
+			for (int w = 0; w < 8; w++) {
+				if (this.Plateau[i][w].getColor().equals("b")) {
+					nb++;
+				}
+			}
+		}
+		return nb;
+	}
+    // Renvoie le nombre de pion noir sur le plateau
+	public int nbPionNoir() {
+		int nb = 0;
+		for (int i = 0; i < 8; i++) {
+			for (int w = 0; w < 8; w++) {
+				if (this.Plateau[i][w].getColor().equals("n")) {
+					nb++;
+				}
+			}
+		}
+		return nb;
+    }
+    public void VoirTableau() {
+        System.out.println("% Etat Initial du plateau de jeu:");
+		System.out.println("% ABCDEFGH");
+		for (int i = 0; i < 8; i++) {
+			System.out.print((i + 1) + " ");
+			for (int j = 0; j < 8; j++) {
+				System.out.print(this.Plateau[i][j].getColor());
+			}
+			System.out.println(" " + (i + 1));
+		}
+		System.out.println("% ABCDEFGH");
+    }
+    
+    public static void main(String[] args) {
+		// String fileimport = "test.txt";
+		PlateauFousFous PlateauTest = new PlateauFousFous();
+        //PlateauTest.saveToFile(fileimport);
+        PlateauTest.VoirTableau();  
+        System.out.print(PlateauTest.finDePartie());
+		//PlateauTest.setFromFile(fileimport);
+		//PlateauTest.VoirTableau();
+		//PlateauTest.play("B1-C2", "blanc");
+		//PlateauTest.VoirTableau();
+		//PlateauTest.estValide("B2-C2", "blanc");
+		//PlateauTest.MovePossible("blanc");
+	}
+}
