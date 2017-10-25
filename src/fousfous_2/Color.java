e998dc734aafda50f76da1657bb9f51124c526ab
monger123 <germond.baptiste@gmail.com>
2017-10-25 21:26:57 +0200

    ajout d'un enum pour la couleur

---
+++ src/fousfous_2/Color.java
@@ -0,0 +1,29 @@
+package fousfous_2;
+
+public enum Color {
+	//Objets directement construits	
+	BLANC ("b"),
+	NOIR ("n"),
+	VIDE ("-");
+
+	private String name = "";
+
+	//Constructeur
+	Color(String name){
+		this.name = name;
+	}
+
+	public String toString() {
+		return name;
+	}
+	
+	// Convertion d'un string en Color
+	public static Color fromString(String text) {
+		for (Color c : Color.values()) {
+			if (c.name.equalsIgnoreCase(text)) {
+				return c;
+			}
+	    }
+	    return null;
+	}
+}
