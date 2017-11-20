60e5d32b25dc85dea30a5b6fe7ea9eeb64d55908
monger123 <germond.baptiste@gmail.com>
2017-11-20 20:42:20 +0100

    Suppression des caractères bizzare

---
+++ src/fousfous_2/JoueurFinal.java
@@ -0,0 +1,40 @@
+package fousfous_2;
+
+public class JoueurFinal implements IJoueur {
+
+	@Override
+	public int getNumJoueur() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String choixMouvement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void declareLeVainqueur(int colour) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouvementEnnemi(String coup) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String binoName() {
+		return "JoueurFinal";
+	}
+
+	@Override
+	public void initJoueur(int mycolour) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
